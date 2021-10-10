732477b58019e4d5b08f8bc8c3b8fc0944d0927b
Arturo Gittens <gittens.arturo@gmail.com>
2021-10-10 16:01:01 -0500

    avances

---
+++ js/lab2in.js
@@ -0,0 +1,14 @@
+var Lab2in = {
+    inputLabel: '.lab2in-label',
+    location: 'https://jsonplaceholder.typicode.com/users',
+
+    init: function(){
+        $(this.inputLabel).on('click', function () {
+            alert('mi primera clase de javascript');
+        });
+    },
+
+    showLabel: function () {
+
+    }
+};
